79d726b313495f0563c52123c9aebd721cedba6c
PC-07 <PC-07@DESKTOP-PQA1J39>
2022-03-30 03:06:13 -0500

    Clase Computadores

---
+++ src/main/java/com/mycompany/poo_unidad01_ccoa/Computador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_unidad01_ccoa;
+
+/**
+ *
+ * @author PC-07
+ */
+public class Computador {
+  String marca;
+  double precio;
+  String Procesador;
+  int Memoria;
+}
