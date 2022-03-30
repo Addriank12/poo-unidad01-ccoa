2d6b0e108a1c1ce3d1c96bcc01f13fd2a0a14108
PC-07 <PC-07@DESKTOP-PQA1J39>
2022-03-30 02:50:09 -0500

    Clase estudientes y metodos

---
+++ src/main/java/com/mycompany/poo_unidad01_ccoa/Estudiante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.poo_unidad01_ccoa;
+/**
+ *
+ * @author PC-07
+ */
+public class Estudiante {
+    String nombre;
+    int calificacion;
+    String carrera;
+    double peso;
+}
